349e7792eef654b3d9d40d274b754d7de9c386b4
thanhnguyenha <133100610+thanhnguyenha@users.noreply.github.com>
2024-05-29 22:04:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-let data = {
+let dataDefault = {
   a2: "",
+  a2class: "",
   b2: "",
+  b2class: "",
   a3: "",
+  a3class: "",
   b3: "", 
+  b3class: "",
   a4: "701 GD1",
-  b4: "701 GD1", 
+  a4class: "17-07",
+  b4: "701 GD1",
+  b4class: "17-08", 
   a5: "",
+  a5class: "",
   b5: "", 
+  b5class: "",
   a6: "801 GD1",
-  b6: "309 GD2", 
+  a6class: "17-03",
+  b6: "309 GD2",
+  b6class: "17-06", 
   a7: "801 GD1",
-  b7: "801 GD1", 
+  a7class: "17-05",
+  b7: "801 GD1",
+  b7class: "17-04", 
 }
+
+let data = {...dataDefault}
 
 const saveData = () => {
   localStorage.setItem("data", JSON.stringify(data));
@@ -29,11 +43,14 @@ const showPopup = (id) => {
   temp = id;
   $("#popup").css("display", "flex");
   $("#input_value").val(data[id])
+  $("#class_value").val(data[`${id}class`])
 }
 
 const saveValue = () => {
+  console.log("temp = ", temp)
   $("#popup").hide();
   data[temp] = $("#input_value").val();
+  data[`${temp}class`] = $("#class_value").val();
   saveData();
   updateUI();
 }
@@ -49,8 +66,15 @@ const updateUI = () => {
 
 const deleteRoom = (id) => {
   data[id] = "";
+  data[`${id}class`] = "";
   updateUI();
   saveData();
+}
+
+const resetData = () => {
+  data = {...dataDefault};
+  saveData();
+  updateUI();
 }
 
 $(document).ready(function () {
